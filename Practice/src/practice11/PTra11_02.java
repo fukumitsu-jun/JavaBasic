18efe55dbc8de5d5a0917077621315ee4d762d94
MYCOMPUTER\bbbb4 <jun.fukumitsu.likeit@gmail.com>
2018-12-12 18:56:14 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,4 +1,7 @@
 package practice11;
+
+import java.util.Scanner;
+
 /*
  * PTra11_02.java
  *   作成	LIKEIT	2017
@@ -10,11 +13,17 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-		 new  ReadFileClass();
+		Book[] books = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
+		String line = new Scanner(System.in).nextLine();
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		for(int i = 0;i < books.length;i++) {
+			if(books[i].title.contains(line)) {
+				System.out.println(books[i].dispBookInfo());
+			}
+		}
 
 	}
 }
