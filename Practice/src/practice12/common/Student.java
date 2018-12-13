ce7100c2d084e301ff92e0c581dd0bc4d044bc8a
MYCOMPUTER\bbbb4 <jun.fukumitsu.likeit@gmail.com>
2018-12-13 14:46:51 +0900

    12-1

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -12,21 +12,45 @@ package practice12.common;
  *
  */
 public class Student {
+	public int getstudentId() {
+	    return this.studentId;
+	}
+
 	/** 受講生ID */
-	public int studentId;
+	private int studentId;
+
+	public String studentName() {
+	    return this.studentName;
+	}
 
 	/** 受講生氏名 */
-	public String studentName;
+	private String studentName;
+
+	public String companyName() {
+	    return this.companyName;
+	}
 
 	/** 会社名 */
-	public String companyName;
+	private String companyName;
+
+	public String className() {
+	    return this.className;
+	}
 
 	/** 教室名 */
-	public String className;
+	private String className;
+
+	public String mail() {
+	    return this.mail;
+	}
 
 	/** メールアドレス */
-	public String mail;
+	private String mail;
+
+	public String password() {
+	    return this.password;
+	}
 
 	/** パスワード */
-	public String password;
+	private String password;
 }
